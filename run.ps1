a6739b5ef41d060f9ff4b88c9db0875fa6a4a90c
yohikofox <yoann.lorho@gmail.com>
2022-10-03 17:24:48 +0200

    reset serviceAccount file

--- run.ps1
+++ run.ps1
@@ -2,5 +2,5 @@ helm repo add sti https://yohikofox.github.io/starinvest-helm/
 
 helm repo update
 
-helm --kubeconfig=kubeconfig --namespace starinvest-staging --set namespace.name=starinvest-staging  upgrade -i yolo-web sti/web --version 0.1.2
+helm --kubeconfig=kubeconfig --namespace starinvest-staging --set namespace.name=starinvest-staging  upgrade -i yolo-web sti/web --version 0.1.4
 # helm --kubeconfig=kubeconfig delete yolo-web
