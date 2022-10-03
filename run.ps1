655e3ad730d210a8711a6edcdd3891a73d34a0c6
yohikofox <yoann.lorho@gmail.com>
2022-10-03 17:08:21 +0200

    fix namespace impl

--- run.ps1
+++ run.ps1
@@ -2,5 +2,5 @@ helm repo add sti https://yohikofox.github.io/starinvest-helm/
 
 helm repo update
 
-helm --kubeconfig=kubeconfig upgrade -i yolo-web sti/web
-helm --kubeconfig=kubeconfig delete yolo-web
+helm --kubeconfig=kubeconfig upgrade -i yolo-web sti/web --version 0.1.1
+# helm --kubeconfig=kubeconfig delete yolo-web
